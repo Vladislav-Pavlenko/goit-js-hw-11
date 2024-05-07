4eddc2ba6c75d277e0765478bb81c3470715b26a
Vladislav-Pavlenko <151663067+Vladislav-Pavlenko@users.noreply.github.com>
2024-05-07 13:47:05 +0300

    3

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,13 +1,14 @@
-export function renderMarkup(params, gallery) {
-  const galleryMarkup = params.reduce((acc, img) => {
-    acc += `<li>
-    <a>
-        <img src="${img.webformatURL}" alt="${img.tags}"  width="360"></a>
-        <p>Likes <span></span>${img.likes}</span></p>
-        <p>Views <span></span>${img.likes}</span></p>
-        <p>Comments <span></span>${img.likes}</span></p>
-        <p>Downloads <span></span>${img.likes}</span></p>
-            </li>`;
+export function renderMarkup(imgArray, gallery) {
+  const galleryMarkup = imgArray.reduce((acc, img) => {
+    return (acc += `<div class="gallery-item">
+          <a href="${img.largeImageURL}"><img src="${img.webformatURL}" alt="${img.tags}"  width="360"/></a>
+          <div class="info">
+              <p>Likes <span></span>${img.likes}</span></p>
+              <p>Views <span></span>${img.views}</span></p>
+              <p>Comments <span></span>${img.comments}</span></p>
+              <p>Downloads <span></span>${img.downloads}</span></p>
+          </div>
+              </div>`);
   }, '');
-  gallery.adjacentHTML('beforeend', galleryMarkup);
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 }
